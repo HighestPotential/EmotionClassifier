c3abc01111dee2bc8f6ae7bac73b311074bbb99d
tiago-hagen <141409370+tiago-hagen@users.noreply.github.com>
2025-12-28 21:22:34 -0800

    Adding the resize and cropping classes, the demo and the handling from dataset to csv

---
+++ preprocessing/resizing_64.py
@@ -0,0 +1,57 @@
+import cv2
+import numpy as np
+from image_processor_interface import ImageProcessor
+from skip_image import SkipImage
+
+
+
+class resizing_to_64(ImageProcessor):
+    """
+    A class that resizes images to 64x64 pixels from a set of images. 
+    For smaller images a cubic interpolation method has to be used.
+    Experiments with the use from SRGAN will be executed.
+
+    Attributes
+    -----------
+
+    None
+
+    Methods
+    --------
+
+    process(self, image: np.ndarray, threshold = 100.0) -> np.ndarray
+        processes n images stored in a numpy array of size (n, h, w) where h and 
+        w are the height and width of the image
+    """
+      
+    def process(self, image: np.ndarray) -> np.ndarray:
+        """
+        Resizes the image to the aimed 64x64 size. 
+        If the image is smaller than 64x64, it uses cubic interpolation to upscale the image.
+        
+
+        :param image: supports (H,W) and (H,W,C) shape
+
+
+
+        :type image: expects a np.ndarray
+
+
+        :return: Returns None when the input "image" is None or empty 
+                Otherwise it returns the 64x64 image
+
+
+        :rtype: ndarray
+        """
+
+        resized_image = image.copy()
+
+        if image is None:
+                raise SkipImage("Input image is None")
+        
+        elif resized_image.size < 64:
+            return cv2.resize(resized_image, (64,64), interpolation=cv2.INTER_CUBIC) 
+        else:    
+            return cv2.resize(resized_image, (64,64))
+    
+ 
