eb69ffaa72437aca39caba4aea3c93a3a2afac7f
Daniel Pfefferkorn <daniel.pfefferkorn2005@gmail.com>
2026-02-21 13:53:15 +0100

    preprocessing cleanup

--- preprocessing/skip_image.py
+++
@@ -1,8 +0,0 @@
-<<<<<<< HEAD
-class SkipImage(Exception):
-    """Raised when an image should be skipped in the pipeline."""
-=======
-class SkipImage(Exception):
-    """Raised when an image should be skipped in the pipeline."""
->>>>>>> main
-    pass
